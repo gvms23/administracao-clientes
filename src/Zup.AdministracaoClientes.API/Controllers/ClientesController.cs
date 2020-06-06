319d4d5f18dfcbfbc0095decf051b3858d4a6e4b
Gabriel Vicente <gabrielvicente.m@gmail.com>
2020-06-06 15:08:27 -0300

    Inserida camada de API.

---
+++ src/Zup.AdministracaoClientes.API/Controllers/ClientesController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Zup.AdministracaoClientes.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ClientesController : ControllerBase
+    {
+        private readonly ILogger<ClientesController> _logger;
+
+        public ClientesController(ILogger<ClientesController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return  new List<string>
+            {
+                "Teste 1",
+                "Teste 2"
+            };
+        }
+    }
+}
---
+++ src/Zup.AdministracaoClientes.API/Program.cs
@@ -0,0 +1,24 @@
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+
+namespace Zup.AdministracaoClientes.API
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder
+                        .UseContentRoot(Directory.GetCurrentDirectory())
+                        .UseIISIntegration()
+                        .UseStartup<Startup>();
+                });
+    }
+}
